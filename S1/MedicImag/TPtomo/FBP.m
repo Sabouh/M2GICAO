610383c2a0229a8261ab18a9e0e202ea66ff517c
Sabouh <bouhsabine@gmail.com>
2015-12-09 15:31:15 +0100

    ohlabackgurl

--- S1/MedicImag/TPtomo/FBP.m
+++ S1/MedicImag/TPtomo/FBP.m
@@ -18,9 +18,38 @@ FG = zeros(Q,P);
 TFG = fft(g');
     for k=1:P
         FG(:,k) = filter' .* TFG(:,k);
-       
+
     end
      GF = (ifft(FG))';
 %end
+
 figure(2);
 imagesc(GF);
+
+figure(3);
+colormap('gray');
+imagesc(GF);
+
+%backprojection
+%phi : nphi
+% k de 1 a P
+% x.teta(phi) : x1.cos(phik) + x2.sin(phik)
+%
+N=200;
+res = 0;
+h = pi/P;
+mu = zeros(N,N);
+%%On se replace ds la bonne base
+for i=1:N
+    for j=1:N
+        for k =1:P
+            xteta = (cos(k*pi/P)*(-1+(i-0.5)*h))+(sin(k*pi/P)*(-1+(j-0.5)*h));
+            %%On interpole xteta !!!!!!
+            res = res + interpole(GF,k,xteta);
+        end
+        mu(i,j) = res;
+        res=0;
+    end
+end
+figure(4);
+imagesc(mu);
